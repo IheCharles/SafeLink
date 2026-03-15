a5c6ef56174698164caa17e149173eede8da83bf
Charles Iheanetu <chihenetu@microsoft.com>
2026-03-14 17:55:25 -0700

    more firebase configs

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,16 +1,19 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyD5IQ3tw9B4Qq5W2-GKd4EnxMOVqS31Nyw",
+  authDomain: "safelink-88497.firebaseapp.com",
+  projectId: "safelink-88497",
+  storageBucket: "safelink-88497.firebasestorage.app",
+  messagingSenderId: "622268402787",
+  appId: "1:622268402787:web:26a5467a89887cb265fc56",
+  measurementId: "G-04MQREPT88",
 };
 
 export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
